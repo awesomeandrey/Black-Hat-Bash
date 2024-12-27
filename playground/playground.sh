300fb7905047694c7ea3176331f2e75ed304ac7e
andrii-kali <awesomeandrey@ukr.net>
2024-12-27 09:31:21 -0500

    WIP Chpt 3

--- playground/playground.sh
+++ playground/playground.sh
@@ -8,24 +8,9 @@
 # alias playground="bash /home/kali/Projects/Black-Hat-Bash/playground/playground.sh"
 
 
-TARGET_NAME=$1
-TARGET_DOMAIN=$2
+#TARGET_NAME="$1"
+#TARGET_DOMAIN="$2"
 
-if [[ -z "$TARGET_NAME" ]] || [[ -z "$TARGET_DOMAIN" ]]; then
-  echo "Mandatory arguments are missing"
-  exit 1
-fi
+fileName=$(basename "$0")
 
-echo "$TARGET_NAME ($TARGET_DOMAIN)"
-
-PING_RESULT="success"
-
-if ping -c 2 "$TARGET_DOMAIN" | grep -q "not known"; then
-  PING_RESULT="failure"
-fi
-
-touch "ping_result.csv"
-currDate=$(date +%m-%d-%Y)
-csvRow=("$TARGET_NAME" "$TARGET_DOMAIN" "$PING_RESULT" "$currDate")
-
-echo "${csvRow[*]}" > "ping_result.csv"
+echo "Basename is $fileName"
